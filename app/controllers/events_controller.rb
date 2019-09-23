21c09121f021d82de9ee12cbed22864234142955
Jinzi Yan <stephanie.jinzi.yan@gmail.com>
2019-09-23 16:26:01 -0700

    add timeline

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,17 @@
 class EventsController < ApplicationController
+    def create
+      start_time = DateTime.parse(params["start_time"])
+      end_time = DateTime.parse(params["end_time"])
+      
+      (start_time..end_time).each do |date|
+      Event.create(time:date, activity_id:params["activity_id"], user_id:params["user_id"])
+      end
+    end
 
+    def index
+        selected_date = DateTime.parse(params["selected_date"]).beginning_of_day
+        events = Event.where('user_id = ? AND time >= ? AND time < ?', params['user_id'], selected_date, selected_date + 1.day)
+        render json: events.to_json(:include => :activity)
+    end
     
 end
